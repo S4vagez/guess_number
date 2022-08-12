99678bd0dc766e0ee68dfdee4675b7a4c4e4af4a
Panuwit Emchom <emchom_p@silpakorn.edu>
2022-08-12 20:15:58 +0700

    Initial commit

---
+++ lib/hello.dart
@@ -0,0 +1,21 @@
+
+import 'dart:io';
+import 'dart:math';
+
+import 'game.dart';
+
+void main() {
+  var game = Game();
+
+  int result = 0;
+  do {
+    stdout.write('PLease guess the number between 1 and 100 : ');
+    var input = stdin.readLineSync();
+    var guess = int.tryParse(input!);
+    if(guess == null){
+      print('Please enter number only');
+      continue;
+    }
+    result = game.doGuess(guess);
+  }while(result != 1);
+}
